cc46befc26955c9f298e4c98a2e082238f3456ff
eagleschool <92178713+eagleschool@users.noreply.github.com>
2021-10-08 14:17:04 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+This calculator is a basic one.
+The first number is addition, second is subtraction, third is multiplication, and fourth is division! I might compile this into a exe so you dont need python.
---
+++ calculator.py
@@ -0,0 +1,14 @@
+# expirement with python
+num1 = input('Enter First Number: ')
+num2 = input('Enter Second Number: ')
+
+result1 = float(num1) + float(num2)
+result2 = float(num1) - float(num2)
+result3 = float(num1) * float(num2)
+result4 = float(num1) / float(num2)
+
+print('Addition: ', result1)
+print('Subtraction: ',result2)
+print('Multiplication: ',result3)
+print('Division: ',result4)
+
